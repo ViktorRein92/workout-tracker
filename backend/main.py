eeecb16f84594d9dd331147c219771fefd94eaf2
ViktorRein92 <viktor_rein@outlook.de>
2026-06-10 14:31:27 +0200

    feat: add read and write functions for workout sessions

--- backend/main.py
+++ backend/main.py
@@ -12,6 +12,17 @@ def create_session(name: str, notes: str):
     print(f"✅ Trainingseinheit '{name}' angelegt!")
     db.close()
 
-create_session("Pull Day", "Habe Rücken trainiert")
+#create_session("Pull Day", "Habe Rücken trainiert")
 
+def get_sessions():
+    db = SessionLocal()
+    sessions = db.query(models.WorkoutSession).all()
+    db.close()
+    return sessions
 
+def print_sessions():
+    sessions = get_sessions()
+    for s in sessions:
+        print(f"✅ ID: {s.id}, Date: {s.date}, Name: {s.name}, Notes: {s.notes}")
+
+print_sessions()
