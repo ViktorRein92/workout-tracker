8017f76ff7a539197779ebba8f5ea89ba55f2f66
ViktorRein92 <viktor_rein@outlook.de>
2026-06-13 20:56:58 +0200

    chore: configure launch.json for uvicorn

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,13 @@
-from backend.database import engine, Base, SessionLocal
+from fastapi import FastAPI
+from backend.database import engine, Base
 from backend import models
 
 Base.metadata.create_all(bind=engine)
-print("✅ Datenbank bereit!")
 
-def create_session(name: str, notes: str):
-    db = SessionLocal() 
-    session = models.WorkoutSession(name=name, notes=notes)
-    db.add(session)
-    db.commit()
-    print(f"✅ Trainingseinheit '{name}' angelegt!")
-    db.close()
+app = FastAPI()
 
-#create_session("Pull Day", "Habe Rücken trainiert")
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
-def get_sessions():
-    db = SessionLocal()
-    sessions = db.query(models.WorkoutSession).all()
-    db.close()
-    return sessions
 
-def print_sessions():
-    sessions = get_sessions()
-    for s in sessions:
-        print(f"✅ ID: {s.id}, Date: {s.date}, Name: {s.name}, Notes: {s.notes}")
-
-print_sessions()
